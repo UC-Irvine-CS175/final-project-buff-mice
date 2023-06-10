d276f3e2da31bb02ee858884cd8ae3b3d88cd7ef
ArmenGadayan <armengadayan@gmail.com>
2023-06-09 19:10:28 -0700

    Adding code to get all data, updated bps_dataset to hot encode 5 labels

--- src/data_utils.py
+++ src/data_utils.py
@@ -53,30 +53,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import pyprojroot
 import sys
-import numpy as np
 import csv
-
 sys.path.append(str(pyprojroot.here()))
 
-class ZoomBPS(object):
-    def __init__(self, zoom: float=1) -> None:
-        self.zoom = zoom
-
-    def __call__(self, image) -> np.ndarray:
-        s = image.shape
-        s1 = (int(self.zoom*s[0]), int(self.zoom*s[1]))
-        img = np.zeros((s[0], s[1]))
-        img_resize = cv2.resize(image, (s1[1],s1[0]), interpolation = cv2.INTER_AREA)
-        # Resize the image using zoom as scaling factor with area interpolation
-        if self.zoom < 1:
-            y1 = s[0]//2 - s1[0]//2
-            y2 = s[0]//2 + s1[0] - s1[0]//2
-            x1 = s[1]//2 - s1[1]//2
-            x2 = s[1]//2 + s1[1] - s1[1]//2
-            img[y1:y2, x1:x2] = img_resize
-            return img
-        else:
-            return img_resize
 
 def get_bytesio_from_s3(
     s3_client: boto3.client, bucket_name: str, file_path: str
@@ -96,8 +75,9 @@ def get_bytesio_from_s3(
     # use the S3 client to read the contents of the file into memory
 
     # create a BytesIO object from the file contents
-
-    return BytesIO( s3_client.get_object(Bucket = bucket_name, Key = file_path ).read() )
+    response = s3_client.get_object(Bucket=bucket_name, Key=file_path)
+    return BytesIO(response["Body"].read())
+    #raise NotImplementedError
 
 
 def get_file_from_s3(
@@ -118,19 +98,21 @@ def get_file_from_s3(
     """
     
     # Create the directory if it does not exist
-    if ( os.path.exists(local_file_path )):
+    if not os.path.exists(local_file_path):
         os.makedirs(local_file_path)
-    
 
     # Create path with local directory provided by the userfile and the name of the s3 file of interest
     # derived from the s3_file_path
-    local_file_name = os.path.join(local_file_path, os.path.basename(s3_file_path))
-    
+    file = os.path.split(s3_file_path)[1]
+    full_path = os.path.join(local_file_path, file)
+    if not os.path.exists(full_path):
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
 
     # Download file
-    s3_client.download_file(bucket_name, s3_file_path, local_file_name)
-    
-    return local_file_name
+    s3_client.download_file(bucket_name, s3_file_path, full_path)
+
+    return full_path
+    #raise NotImplementedError
 
 
 def save_tiffs_local_from_s3(
@@ -155,26 +137,15 @@ def save_tiffs_local_from_s3(
       None
     """
     # Get s3_file_paths from local_fnames_meta_path csv file
-    filenames = []
-    with open(local_fnames_meta_path, 'r') as files:
-        reader = csv.reader(files)
-        # all files have smth like filename,dose_Gy,particle_type,hr_post_exposure
-        # so skip the first row!
-        filenames = [ row[0] for row in reader[1:] ]
-
-
+    df= pd.read_csv(local_fnames_meta_path, usecols=["filename"])
+    file_names = df["filename"]
 
     # Download files after constructing s3 full paths including the filenames from the csv file
-    
-    for filename in filenames:
-        s3_file_path = os.path.join( s3_path, filename )
-        local_file_path = os.path.join( save_file_path, filename )
+    # Call get_file_from_s3 function for each s3_file_path in s3_file_paths
+    for fn in file_names:
+        get_file_from_s3(s3_client, bucket_name, s3_path + "/" + fn, save_file_path)
 
-        # Call get_file_from_s3 function for each s3_file_path in s3_file_paths 
-        get_file_from_s3( s3_client, bucket_name, s3_file_path, local_file_path )
-
-    
-
+    #raise NotImplementedError
 
 
 def export_subset_meta_dose_hr(
@@ -201,36 +172,48 @@ def export_subset_meta_dose_hr(
       file
     """
     # Create output directory out_dir_csv if it does not exist
-    if ( os.path.exists( out_dir_csv )):
-        os.makedirs( out_dir_csv )
-
+    if not os.path.exists(out_dir_csv):
+        os.makedirs(out_dir_csv)
     
     # Load csv file into pandas DataFrame
-    pandas_date_frame = pd.read_csv(in_csv_path_local)
-
+    df = pd.read_csv(in_csv_path_local)
 
     # Check that dose_Gy and hr_post_exposure_val are valid
 
     #               low, med, hi
     # Fe dose_Gy = [0.0, 0.3, 0.82]
     # Xray dose_Gy = [0.0, 0.1, 1.0]
-    # go through each row, -2 tells if Fe and -1 tells if its valid or smth
-    # what to do if its not?
+    
+    if ((df["particle_type"] == "Fe") & (~df["dose_Gy"].isin([0.0, 0.3, 0.82]))).any():
+        raise Exception("One or more Fe dose values are not valid")
+    
+    if ((df["particle_type"] == "X-ray") & (~df["dose_Gy"].isin([0.0, 0.1, 1.0]))).any():
+        raise Exception("One or more X-ray dose values are not valid")
+    
+    if ((~df["hr_post_exposure"].isin([4, 24, 48]))).any():
+        raise Exception("One or more exposure values are not valid")
 
 
     # Slice DataFrame by attributes of interest
-
+    Fe_dose = {'low': 0.0, 'med': 0.3, 'hi': 0.82}
+    Xray_dose = {'low': 0.0, 'med': 0.1, 'hi': 1.0}
+    filtered_data = df[(df["hr_post_exposure"] == hr_post_exposure_val) & ((df["dose_Gy"] == Fe_dose[dose_Gy_specifier]) & (df["particle_type"] == "Fe") 
+                                                                           | (df["dose_Gy"] == Xray_dose[dose_Gy_specifier]) & (df["particle_type"] == "X-ray"))]
 
     # Write sliced DataFrame to output csv file with same name as input csv file with 
     # _dose_hr_post_exposure.csv appended
-   
+    file = os.path.splitext(in_csv_path_local)[0]
+    new_file_path = os.path.join(out_dir_csv, file + "_dose_" + dose_Gy_specifier + "_hr_" + str(hr_post_exposure_val) + "_post_exposure.csv")
+
     # Construct output csv file path using out_dir_csv and the name of the input csv file
     # with the dose_Gy and hr_post_exposure_val appended to the name of the input csv file
     # for data versioning. 
+    filtered_data.to_csv(new_file_path)
 
     # Write sliced DataFrame to output csv file with name constructed above
-    return ("", 1)
-    raise NotImplementedError
+
+    return(new_file_path, filtered_data.shape[0])
+    #raise NotImplementedError
     
 def train_test_split_subset_meta_dose_hr(
         subset_meta_dose_hr_csv_path: str,
@@ -260,27 +243,37 @@ def train_test_split_subset_meta_dose_hr(
         Tuple[str, str]: a tuple of the output csv file paths for the train and test sets
     """
     # Create output directory out_dir_csv if it does not exist
+    if not os.path.exists(out_dir_csv):
+        os.makedirs(out_dir_csv)
 
     # Load csv file into pandas DataFrame and use the train_test_split function to split the
     # DataFrame into train and test sets
- 
+    df = pd.read_csv(subset_meta_dose_hr_csv_path)
+    train, test = train_test_split(df, test_size=test_size, random_state=random_state, stratify=df[stratify_col])
 
     # Rewrite index numbers for both train and test sets to conform to order in new dataframe
     # (otherwise, index numbers will be out of order)
+    train.reset_index(inplace=True, drop=True)
+    test.reset_index(inplace=True, drop=True)
 
     # Write train and test DataFrames to output csv files with same name as input csv file with
     # _train.csv or _test.csv appended
+    train_file_path = os.path.join(out_dir_csv, os.path.splitext(subset_meta_dose_hr_csv_path)[0] + "_train.csv")
+    test_file_path = os.path.join(out_dir_csv, os.path.splitext(subset_meta_dose_hr_csv_path)[0] + "_test.csv")
+    train.to_csv(train_file_path)
+    test.to_csv(test_file_path)
 
     # return the train and test csv paths
-    return ("", "")
-    return NotImplementedError
+    return (train_file_path, test_file_path)
+    #return NotImplementedError
 
 def main():
     """
     A driver function for testing the functions in this module. Use if you like.
     """
 
-    output_dir = '../data/processed'
+    # output_dir = '../data/processed'
+    output_dir = os.path.join("..","data","processed")
 
     # s3 bucket info
     bucket_name = 'nasa-bps-training-data'
@@ -291,37 +284,37 @@ def main():
     # local file path info
     local_file_path = "../data/raw"
 
-    local_new_path_fname = get_file_from_s3(
-        s3_client=s3_client,
-        bucket_name=bucket_name,
-        s3_file_path=s3_meta_csv_path,
-        local_file_path=local_file_path)
+    # local_new_path_fname = get_file_from_s3(
+    #     s3_client=s3_client,
+    #     bucket_name=bucket_name,
+    #     s3_file_path=s3_meta_csv_path,
+    #     local_file_path=local_file_path)
 
-    subset_new_path_fname, subset_size = export_subset_meta_dose_hr(
-        dose_Gy_specifier='hi',
-        hr_post_exposure_val=4,
-        in_csv_path_local=local_new_path_fname,
-        out_dir_csv=output_dir)
+    # subset_new_path_fname, subset_size = export_subset_meta_dose_hr(
+    #     dose_Gy_specifier='hi',
+    #     hr_post_exposure_val=4,
+    #     in_csv_path_local=local_new_path_fname,
+    #     out_dir_csv=output_dir)
 
-    print(subset_size)
+    # print(subset_size)
 
-    subset_new_path_fname, subset_size = export_subset_meta_dose_hr(
-        dose_Gy_specifier='med',
-        hr_post_exposure_val=4,
-        in_csv_path_local=local_new_path_fname,
-        out_dir_csv=output_dir)
+    # subset_new_path_fname, subset_size = export_subset_meta_dose_hr(
+    #     dose_Gy_specifier='med',
+    #     hr_post_exposure_val=4,
+    #     in_csv_path_local=local_new_path_fname,
+    #     out_dir_csv=output_dir)
     
-    print(subset_size)
+    # print(subset_size)
 
-    subset_new_path_fname, subset_size = export_subset_meta_dose_hr(
-        dose_Gy_specifier='low',
-        hr_post_exposure_val=4,
-        in_csv_path_local=local_new_path_fname,
-        out_dir_csv=output_dir)
+    # subset_new_path_fname, subset_size = export_subset_meta_dose_hr(
+    #     dose_Gy_specifier='low',
+    #     hr_post_exposure_val=4,
+    #     in_csv_path_local=local_new_path_fname,
+    #     out_dir_csv=output_dir)
     
 
     train_test_split_subset_meta_dose_hr(
-        subset_meta_dose_hr_csv_path=subset_new_path_fname,
+        subset_meta_dose_hr_csv_path= r"C:\Users\armen\A CS 175\final-project-buff-mice\data\processed\meta.csv",
         test_size=0.2,
         out_dir_csv=output_dir,
         random_state=42,
